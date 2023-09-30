9d535b341aedf4031c6322f6bb95b07202579d74
Psamcyite <amojoayokunle@gmail.com>
2023-09-30 04:36:52 +0000

    single quotes/ default parameters and return number of args

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,10 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    // No need to modify the variables here
+  }
+
+  return [task, task2];
+}
